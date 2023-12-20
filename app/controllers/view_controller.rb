4480b4c78131674f59f6f6082113429f09740837
tianlitao <2423806988@qq.com>
2023-12-20 17:41:35 +0800

    modify local

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -4,7 +4,11 @@ class ViewController < ActionController::Base
 
   def set_i18n
     if params[:local].present? && params[:local].in?(['zh', 'en'])
-      I18n.locale = params[:local].to_sym
+      cookies[:local] = params[:local]
+    end
+
+    if cookies[:local].present? && cookies[:local].in?(['zh', 'en'])
+      I18n.locale = cookies[:local].to_sym
     end
   end
 
